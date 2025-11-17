61e7c7e661b74fc3471662f1afe474cad4e803d5
Keiron <keiron.dang@gmail.com>
2025-11-18 00:19:08 +0700

    feat: add CORS middleware with configurable options

---
+++ middlware/cors.mw.go
@@ -0,0 +1,36 @@
+package middlware
+
+import (
+	"net/http"
+
+	"github.com/go-chi/cors"
+
+	tul "github.com/kreon-core/shadow-cat-common"
+)
+
+type CORSConfig struct {
+	AllowedOrigins   *[]string
+	AllowedMethods   *[]string
+	AllowedHeaders   *[]string
+	ExposedHeaders   *[]string
+	AllowCredentials *bool
+	MaxAge           *int
+}
+
+func CORS(cfg *CORSConfig) func(next http.Handler) http.Handler {
+	return cors.Handler(cors.Options{
+		AllowedOrigins: tul.OrElse(cfg.AllowedOrigins, []string{"https://*", "http://*"}),
+		AllowedMethods: tul.OrElse(cfg.AllowedMethods, []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}),
+		AllowedHeaders: tul.OrElse(cfg.AllowedHeaders, []string{
+			"Origin",
+			"Accept",
+			"Content-Type",
+			"Authorization",
+			"X-Real-IP",
+			"X-Request-ID",
+		}),
+		ExposedHeaders:   tul.OrElse(cfg.ExposedHeaders, []string{"Content-Length"}),
+		AllowCredentials: tul.OrElse(cfg.AllowCredentials, false),
+		MaxAge:           tul.OrElse(cfg.MaxAge, 300),
+	})
+}
